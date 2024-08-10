919f94c52f9f982bb90fae1aad1fadd762e9d349
Shaima <shaimafuadi45@gmail.com>
2024-08-11 00:47:24 +0300

    التاريخ وصباح ومساء

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,19 @@
+<script lang="ts">
+	import dayjs from 'dayjs';
+
+
+let period = dayjs().format('a') == 'am' ? 'morning' : 'evening';
+</script>
+
 <div class="p-16">
   <header class="flex justify-between">
     <div>
-      <h1 class="text-4xl text-surface-900 mb-2">Good morning👋🏻</h1>
-      <h2 class="text-2xl text-surface-900/50">Today, Saturday 10</h2>
+      <h1 class="text-4xl text-surface-900 mb-2">Good {period}👋🏻</h1>
+      <h2 class="text-2xl text-surface-900/50">
+		 Today, {dayjs().format('dddd D MMM YYYY')}
+	</h2>
     </div>
 
-    <div class="bg-red-500 w-32 h-32"></div>
+    <div class="bg-green-800 w-32 h-12"></div>
   </header>
 </div>
