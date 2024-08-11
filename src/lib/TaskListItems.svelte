7e7e3baa8b3eb23282380995ad09adbe6170db14
Shaima <shaimafuadi45@gmail.com>
2024-08-11 09:18:10 +0300

    اضافة وحذف المهام

--- src/lib/TaskListItems.svelte
+++ src/lib/TaskListItems.svelte
@@ -1,52 +1,85 @@
 <script lang="ts">
-    import dayjs from "dayjs";
-    import { tasks } from "$lib/stores/tasks";
-    import relativeTime from 'dayjs/plugin/relativeTime'
-  
-    dayjs.extend(relativeTime);
+  import dayjs from "dayjs";
+  import { tasks } from "$lib/stores/tasks";
+  import relativeTime from "dayjs/plugin/relativeTime";
+  import { getModalStore, type ModalSettings } from '@skeletonlabs/skeleton';
+			
+  const modalStore = getModalStore();
+  dayjs.extend(relativeTime);
 
-    export let doneTasks : boolean;
+  //console.log('response:', r),
+
+  export let doneTasks: boolean;
+
+  function confirmDelete(task : Task){
+   
+
+       const modal: ModalSettings = {
+	type: 'confirm',
+	// Data
+	title: 'Are you sure you want to delete?',
+	body: `The task: "${task.title}" will be deleted`,
+
+
+  buttonTextCancel : "Cancel",
+  buttonTextConfirm : "Confirm",
+
+  // TRUE if confirm pressed, FALSE if cancel pressed
+	response: (r: boolean) => {
+ if (r) {
+    tasks.update((currentTasks) => {
+      let index = currentTasks.indexOf(task);
+      currentTasks.splice(index,1);
+      return currentTasks;
+    })
+ }
+  },
   
-  
-  </script>
+};
+modalStore.trigger(modal);
+    
+
+  }
+</script>
+
+
 
 {#each $tasks as task}
-{#if task.isDone == doneTasks}
-<li
-  class="bg-secondary-500 p-2 rounded-lg flex justify-between items-center"
->
-  <div>
-    <input bind:checked={task.isDone} class="checkbox rounded-md" type="checkbox" />
+  {#if task.isDone == doneTasks}
+    <li
+      class="bg-secondary-500 p-2 rounded-lg flex justify-between items-center"
+    >
+      <div>
+        <input
+          bind:checked={task.isDone}
+          class="checkbox rounded-md"
+          type="checkbox"
+        />
 
-    <span class="ml-2"> {task.title} </span>
-  </div>
+        <span class="ml-2"> {task.title} </span>
+      </div>
 
-  <div class="flex gap-2">
-    <button class="btn bg-surface-50 hover:bg-surface-300">
-      { dayjs().to(dayjs(task.assignedDate))}
-    </button>
+      <div class="flex gap-2">
+        <button class="btn bg-surface-50 hover:bg-surface-300">
+          {dayjs().to(dayjs(task.assignedDate))}
+        </button>
 
-    <button class="btn bg-surface-50 hover:bg-surface-300">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1em"
-        height="1em"
-        viewBox="0 0 16 16"
-        ><g
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="1.5"
-          ><circle cx="8" cy="2.5" r=".75" /><circle
-            cx="8"
-            cy="8"
-            r=".75"
-          /><circle cx="8" cy="13.5" r=".75" /></g
-        ></svg
-      >
-    </button>
-  </div>
-</li>
-{/if}
+
+        <!-- ?\زر الحذف  -->
+        <button 
+        on:click={() => confirmDelete(task)}
+        class="btn bg-surface-50 hover:bg-surface-300">
+          <svg
+            class="w-5 h-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            ><path
+              fill="currentColor"
+              d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6z"
+            /></svg
+          >
+        </button>
+      </div>
+    </li>
+  {/if}
 {/each}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
-	import '../app.postcss';
+  import "../app.postcss";
+  import { initializeStores, Modal } from '@skeletonlabs/skeleton';
 
-	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+  initializeStores();
+
+  
+
+  // Floating UI for Popups
+  import {
+    computePosition,
+    autoUpdate,
+    flip,
+    shift,
+    offset,
+    arrow,
+  } from "@floating-ui/dom";
+  import { storePopup } from "@skeletonlabs/skeleton";
+  storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
+
+<Modal buttonPositive = "variant-filled-error" />
 <slot />
+
+
